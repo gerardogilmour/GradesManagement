4d83039146cac20dd1971622454f5178e8b8df8c
gerardogilmour <gerardogilmour@outlook.com>
2016-02-01 13:25:52 -0600

    First commit including basic files, and solution related files

---
+++ GradesManagement/scripts/default.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    this.gradesControl = this.gradesControl || {};
+    var ns = this.gradesControl;
+
+    //TODO ADD CREATE ASSIGMENTS MODULE
+    function createAssigment() {
+
+    };
+
+    //TODO CREATE ADD ALUMN FUNCTION, EDIT ALUMN, DELETE ALUMN (SUGGESTED USE PROTOTYPES)
+
+})();
+
